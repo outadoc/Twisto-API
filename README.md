5c755653dfe19fdb5337eb3b5c9a8f0f77513785
outadoc <outadoc@gmail.com>
2013-09-28 23:47:36 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,147 @@
+# Twisto/Timeo Real-time API
+
+## Introduction
+
+Le réseau Twisto est le réseau de bus et tram de la ville de Caen, géré par Viacités. Malheureusement, ce syndicat ne propose pas d'API pour accéder à ses services qui se vantent pourtant d'être disponibles sur un maximum d'appareils possible.
+
+Cette API non-officielle comble partiellement ce manque en vous permettant, notamment, d'obtenir en temps réel et au format JSON les horaires des prochains bus Twisto, de la ville de Caen.
+
+Vous pourrez également lister les lignes de bus, directions et arrêts disponibles.
+
+## Fonctionnement
+
+L'API est écrite en PHP et repose sur le système Timeo de Actigraph, disponible sur les sites officiels de Twisto et les applications mobiles.
+
+## Utilisation
+
+Appelez le script `twisto-api.php` avec différentes variables GET en fonction du résultat souhaité.  
+Le résultat est renvoyé sous forme d'un string JSON.
+
+### Lister les lignes de bus/tram
+
+	twisto-api.php?func=getLines
+
+Exemple de résultat :
+
+	[
+		{"id":"TRAM","name":"TRAM"},
+		{"id":"1","name":"Lianes 1"},
+		{"id":"2","name":"Lianes 2"},
+		{"id":"3","name":"Lianes 3"},
+		{"id":"4","name":"Lianes 4"},
+		{"id":"5","name":"Ligne 5"},
+		{"id":"6","name":"Ligne 6"},
+		{"id":"7","name":"Ligne 7"},
+		{"id":"8","name":"Ligne 8"},
+		{"id":"9","name":"Ligne 9"},
+		...
+		{"id":"NUIT","name":"Ligne Noctibus"}
+	]
+
+L'identifiant obtenu pour chaque ligne pourra servir pour les autres fonctions du script.
+
+----------------------------------------
+
+### Lister les directions pour une ligne donnée
+
+	twisto-api.php?func=getDirections&line=XX
+
+...où XX est l'identifiant de la ligne.
+
+Exemple de résultat :
+
+	[
+		{"id":"A","name":"Cuverville mairie"},
+		{"id":"R","name":"Bretteville l'enclos"}
+	]
+
+L'identifiant est A pour "aller", ou R pour "retour".
+
+----------------------------------------
+
+### Lister les arrêts pour une ligne et une direction données
+
+	twisto-api.php?func=getStops&line=XX&direction=A|R
+
+...où XX est l'identifiant de la ligne, et la direction est A ou R.
+
+Exemple de résultat :
+
+	[
+		{"id":"5421","name":"50 acres"},
+		{"id":"11","name":"Bibliotheque"},
+		{"id":"4072","name":"Bois claquet"},
+		{"id":"4082","name":"Briere"},
+		{"id":"3532","name":"Carrefour de la liberte"},
+		{"id":"4112","name":"Charmettes"},
+		{"id":"4122","name":"Clair soleil"},
+		{"id":"511","name":"Creux au renard"},
+		{"id":"1892","name":"Demi-lune"},
+		{"id":"121","name":"Demoge"},
+		{"id":"2052","name":"Edmond rostand"},
+		{"id":"5461","name":"Eglise de bretteville"},
+		...
+		{"id":"4392","name":"Vallee barrey"}
+	]
+
+----------------------------------------
+
+### Lister les prochains bus pour UNE ligne, UNE direction et UN arrêt
+
+	twisto-api.php?func=getSchedule&line=XX&direction=A|R&stop=XXXX
+
+...où XX est l'identifiant de la ligne, la direction est A ou R et XXXX est l'identifiant de l'arrêt.
+
+Exemple de résultat :
+
+	[
+		{
+			"line":"Ligne 11",
+			"direction":"Cuverville mairie",
+			"stop":"Arrêt 50 acres",
+			"next":[
+				"Dans 15 minutes", 
+				"Dans 35 minutes"
+			]
+		}
+	]
+
+----------------------------------------
+
+### Lister les prochains bus pour plusieurs lignes/directions/arrêts à la fois
+
+Pour cela, on envoie directement un cookie (le même que celui utilisé par le système officiel).  
+Le cookie est de la forme `ARRÊT|LIGNE|DIRECTION;ARRÊT|LIGNE|DIRECTION;...`, avec les pipes et points-virgules bien évidemment URLencoded.
+
+Exemple de requête :
+
+	twisto-api.php?func=getSchedule&data=5421%7C11%7CA%3B12%7C11%7CR%3B251%7C3%7CA
+
+Exemple de résultat :
+
+	[
+		{
+			"line":"Ligne 11",
+			"direction":"Cuverville mairie",
+			"stop":"Arrêt 50 acres",
+			"next":[
+				"Dans 15 minutes", 
+				"Dans 35 minutes"
+			]
+		}, {
+			"line":"Ligne 11",
+			"direction":"Bretteville l'enclos",
+			"stop":"Arrêt Bibliotheque",
+			"next":[
+				"Dans 3 minutes", 
+				"Dans 25 minutes"
+			]
+		}, {
+			"line":"Lianes 3",
+			"direction":"Herouville st-clair",
+			"stop":"Arrêt Bicoquet",
+			"next": [
+				"À 0 H 25"
+			]
+		}
+	]
---
+++ twisto-api.php
@@ -0,0 +1,189 @@
+<?php 
+	/* 
+	 *  Twisto real-time API v0.1
+	 *  (c) outa[dev] 2013
+	 *
+	 *  timeo.php
+	 */
+
+	define('API_BASE_URL', 'http://dev.actigraph.fr/actipages/twisto/module/mobile/pivk/relais.html.php');
+	
+	//this gets a distant page with curl, via GET and with a cookie (it's always better with cookies)
+	function getDistantPage($fields, $cookie) {
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, API_BASE_URL . '?' . $fields);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_COOKIESESSION, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			'Cookie: als=' . urlencode($cookie)
+		));
+
+		$server_output = curl_exec($ch);
+
+		curl_close($ch);
+		return $server_output;
+	}
+
+	//this does the same, but for POST method and with no cookies :(
+	function postDistantPage($fields) {
+		$ch = curl_init();
+
+		if($fields == null) {
+			$fields = array();
+		}
+
+		curl_setopt($ch, CURLOPT_URL, API_BASE_URL);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($fields));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+		$server_output = curl_exec($ch);
+
+		curl_close($ch);
+		return $server_output;
+	}
+
+	//we already have a cookie containing a list of the lines we want to get schedules for; send it, parse it, return a JSON object with the full schedule.
+	//the cookie is of the form STOP|LINE|DIRECTION;STOP|LINE|DIRECTION;...
+	function getScheduleFromCookie($cookie) {
+		$content = getDistantPage("a=refresh&borne=affiche_borne&ran=1", $cookie);
+		$schedule;
+		$final = array();
+
+		if($content != null && $content != '') {
+			$regex = "/timeo_ligne_nom'>([a-zA-Z0-9- ']+).+timeo_titre_direction'>([a-zA-Z0-9- ']+).+timeo_titre_arret'>([a-zA-Z0-9&; '-]+).+\n.+\n.+\n.+\n((\s<li id='h[0-9]' class='timeo_horaire'>([a-zA-Z0-9&; '-]+)<\/li>\n)*)/";
+			preg_match_all($regex, $content, $schedule, PREG_SET_ORDER);
+
+			if($schedule != null) {
+				for($i = 0; $i < count($schedule); $i++) {
+					for($j =  0; $j < count($schedule[$i]); $j++) {
+						switch ($j) {
+							case 1:
+								$final[$i]['line'] = $schedule[$i][$j];
+								break;
+							case 2:
+								$final[$i]['direction'] = $schedule[$i][$j];
+								break;
+							case 3:
+								$final[$i]['stop'] = $schedule[$i][$j];
+								break;
+							case 4:
+								preg_match_all("/<li id='h[0-9]' class='timeo_horaire'>([a-zA-Z0-9&; -]+)<\/li>/", $schedule[$i][$j], $schedule[$i][$j]);
+								
+								if($schedule[$i][$j][1] != null) {
+									$final[$i]['next'] = $schedule[$i][$j][1];
+								}
+
+								break;
+						}
+					}
+				}
+
+				echo html_entity_decode(json_encode($final));
+			}	
+		}
+	}
+
+	//this basically is an alias to getScheduleFromCookie that allows us to search for the next buses for a line, direction, stop instead of a raw cookie.
+	//only works for one stop at a time.
+	function getScheduleFromDetails($line, $direction, $stop) {
+		getScheduleFromCookie($stop . '|' . $line . '|' . $direction);
+	}
+
+	//this returns a list of all the available bus lines
+	function getLines() {
+		$content = postDistantPage(null);
+		$lines;
+		$final = array();
+
+		//returns a piece of HTML: parse it to only get insteresting info
+		preg_match_all("/<option value='([a-zA-Z0-9]+)'>([a-zA-Z0-9 ]+)<\/option>/", $content, $lines, PREG_SET_ORDER);
+
+		if($lines != null) {
+			for($i = 0; $i < count($lines); $i++) {
+				for($j =  0; $j < count($lines[$i]); $j++) {
+					switch ($j) {
+						case 1:
+							$final[$i]['id'] = $lines[$i][$j];
+							break;
+						case 2:
+							$final[$i]['name'] = $lines[$i][$j];
+							break;
+					}
+				}
+			}
+
+			echo html_entity_decode(json_encode($final));
+		}
+	}
+
+	//this gives us the available directions (A or R) for a specific line
+	function getDirection($line) {
+		$content = postDistantPage(array("a" => "refresh_list", "ligne" => $line));
+		$directions;
+		$final = array();
+
+		//this returns a piece of JS code: we only want some of the info
+		preg_match_all("/Array\('[A|R]','([a-zA-Z0-9\\\\\-' ]+)'\);/", $content, $directions);
+
+		if($directions != null && $directions[0] != null) {
+			$final[0]['id'] = 'A';
+			$final[0]['name'] = str_replace("\\", '', $directions[1][0]);
+
+			$final[1]['id'] = 'R';
+			$final[1]['name'] = str_replace("\\", '', $directions[1][1]);
+
+			echo html_entity_decode(json_encode($final));
+		}
+	}
+
+	//this gives the available bus stops for a specific line/direction
+	function getStops($line, $direction) {
+		$content = postDistantPage(array("a" => "refresh_list", "ligne_sens" => $line . '_' . $direction));
+		$stops;
+		$final = array();
+
+		preg_match_all("/Array\('([\-_0-9]+)','([a-zA-Z0-9\\\\\-' ]+)'\);/", $content, $stops, PREG_SET_ORDER);
+
+		if($stops != null) {
+			for($i = 0; $i < count($stops); $i++) {
+				for($j =  0; $j < count($stops[$i]); $j++) {
+					switch ($j) {
+						case 1:
+							$expl = explode('_', $stops[$i][$j]);
+							$final[$i]['id'] = $expl[1];
+							break;
+						case 2:
+							$final[$i]['name'] = str_replace("\\", '', $stops[$i][$j]);
+							break;
+					}
+				}
+			}
+
+			echo html_entity_decode(json_encode($final));
+		}
+	}
+	
+	//check what we want to get
+	//if we want the full schedule
+	if($_GET['func'] == "getSchedule") {
+		if(isset($_GET['data'])) {
+			//if we already have a formed cookie
+			getScheduleFromCookie($_GET['data']);
+		} else if(isset($_GET['line']) && isset($_GET['direction']) && isset($_GET['stop'])) {
+			//else, if we're specifying line, direction and stop of the desired schedule
+			getScheduleFromDetails($_GET['line'], $_GET['direction'], $_GET['stop']);
+		}
+	} else if($_GET['func'] == "getLines") {
+		//if we want to list the available lines
+		getLines();
+	} else if($_GET['func'] == "getDirections" && isset($_GET['line'])) {
+		//if we want to list the available directions
+		getDirection($_GET['line']);
+	} else if($_GET['func'] == "getStops" && isset($_GET['line']) && isset($_GET['direction'])) {
+		//if we want to list the available bus stops
+		getStops($_GET['line'], $_GET['direction']);
+	}
+
+?>
